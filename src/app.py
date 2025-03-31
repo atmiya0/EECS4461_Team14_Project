3f4505d090c6382633323e4ddc3eb33c39640f71
Atmiya Jadvani <atmiyajadvani09@gmail.com>
2025-03-31 16:34:13 -0400

    Updated app.py

--- src/app.py
+++ src/app.py
@@ -9,20 +9,46 @@ from mesa.visualization import (
 from model import MetricNetwork  
 from agents import State, TOPICS  
 
+BOT_TOPICS = {"Topic 1", "Topic 2", "Topic 3"}
+HUMAN_TOPICS = {"Topic 4", "Topic 5", "Topic 6"}
 
 def agent_portrayal(agent):
-    node_color_dict = {
-        State.HUMAN: TOPICS.get(agent.topic),  
-        State.BOT: TOPICS.get(agent.topic), 
-        State.INACTIVE_BOT: "gray",  
+    node_color_dict ={
+        State.HUMAN: TOPICS.get(agent.topic),
+        State.BOT: TOPICS.get(agent.topic),
+        State.INACTIVE_BOT: "gray",
     }
 
     return {"color": node_color_dict[agent.state], "size": 45, "marker": "s" if agent.state == State.HUMAN else "o"}
 
-def get_algorithm_topic(model):
+def get_dominating_agent(model):
+    """Display which agent is dominating"""
+    if model.c ==0:
+        return solara.Markdown("##<span style='font-weight: bold'>Simulation has not started</span><br>")
+
+    me = model.most_engaged_topic()
+    current_bot_topic=getattr(model, "bot_topic", None)
+
+    if me ==current_bot_topic:
+        m = f"##<span style='font-weight: bold'>Bots are dominating</span><br>"
+    else:
+        m = f"##<span style='font-weight: bold'>Humans are dominating</span><br>"
+
+    return solara.Markdown(m)
+
+
+def get_status(model):
+    """Display topics being pushed by bots and alogrthim"""
     algorithm_text = getattr(model, "algorithm_topic", "None")
+    algorithm2_text = getattr(model, "algorithm2_topic", "None")
+    bot_topic = getattr(model, "bot_topic", "None")
+
     return solara.Markdown(
-        f"##Algorithm Recommends: {algorithm_text}"
+        f"""
+        ##<span style='font-weight: bold'>Main Algorithm Recommends:{algorithm_text}</span><br>
+        ##<span style='font-weight: bold'>Algorithm 2 Recommends: {algorithm2_text}</span><br>
+        ##<span style='font-weight: bold'>Current Bot Topic:{bot_topic}</span>
+        """,
     )
 
 
@@ -35,39 +61,46 @@ model_params = {
         step=1
     ),
     "active_bot": Slider(
-        label="Active Bots",
+        label="Decision Bots",
         value=2,
         min=1,
         max=10,
         step=1
     ),
     "inactive_bot": Slider(
-        label="Inactive Bots",
+        label="Follower Bots",
         value=3,
         min=1,
         max=10,
         step=1
     ),
     "bot_spread_chance": Slider(
-        label="Bot Spread on Humans",
+        label="Bot on Human Influence",
         value=0.5,
-        min=0.1,
-        max=0.6,
-        step=0.1
+        min=0.0,
+        max=1,
+        step=0.05
     ),
     "human_spread_chance": Slider(
-        label="Human Spread on Humans",
+        label="Human on Human Influence",
         value=0.3,
-        min=0.1,
-        max=0.5,
-        step=0.1
+        min=0.0,
+        max=1,
+        step=0.05
     ),
     "algorithm_spread_chance": Slider(
-        label="Algorithm Spread on Humans",
-        value=0.2,
-        min=0.1,
-        max=0.6,
-        step=0.1
+        label="Algorithm on Human Influence",
+        value=0.5,
+        min=0.0,
+        max=1,
+        step=0.05
+    ),
+    "algorithm_spread_chance2": Slider(
+        label="Algorithm 2 on Human Influence",
+        value=0.05,
+        min=0.0,
+        max=0.1,
+        step=0.01
     ),
 }
 
@@ -92,16 +125,17 @@ model1 = MetricNetwork()
 page = solara.Column(
     [
         solara.Markdown(r'''
-        # <span style="color: orange; font-weight: bold; margin-right: 20px;">Topic 1</span> <span style="color: blue; font-weight: bold; margin-right: 20px;">Topic 2</span>   <span style="color: purple; font-weight: bold;">Topic 3</span>
+        # <span style="color: orange; font-weight: bold;">Topic 1</span> <span style="color: blue; font-weight: bold;">Topic 2</span>   <span style="color: purple; font-weight: bold;">Topic 3</span> <span style="color: red; font-weight: bold;">Topic 4</span> <span style="color: pink; font-weight: bold;">Topic 5</span> <span style="color: green; font-weight: bold;">Topic 6</span>
         ## <span style="font-weight:bold"> Humans (Squares) </span>
         ## <span style="font-weight:bold"> Bots (Circle) </span>
-        ### <span style="margin-left: 35px""> - Inactive Bots (Gray Circle) </span>'''),
+        ### <span style="margin-left: 35px""> - Follower Bots (Gray Circle) </span>'''),
         SolaraViz(
             model1,
             components=[
                 SpacePlot,
                 StatePlot,
-                get_algorithm_topic],
+                get_status,
+                get_dominating_agent],
             model_params=model_params,
             name="Metric Manipulation",
         ),
